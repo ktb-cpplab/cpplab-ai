034dbb54768122a5c735d867565eb635ce55d15b
rkfcl <kwmkr6@gmail.com>
2024-11-05 17:15:49 +0900

    feat: ai 환경변수 전달

--- project/main.py
+++ project/main.py
@@ -1,18 +1,20 @@
-# main.py
+# uvicorn main:app --reload
 from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+
 import os
+
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_teddynote import logging
 from langchain import hub
-
 logging.langsmith('cpplab_test')
 
 prompt_gen_project = hub.pull("cpplab")
-llm = ChatOpenAI(model="gpt-4o-mini")  # 온도 튜닝
-chain_gen_project = prompt_gen_project | llm
+llm = ChatOpenAI(model="gpt-4o-mini") # 온도 튜닝
+chain_gen_project = prompt_gen_project|llm
+# print(prompt_gen_project)
 
 app = FastAPI()
 
@@ -67,11 +69,11 @@ def health_check():
 @app.post('/ai/genproject')
 def genProject(userinfo: UserInfo):
     proj = chain_gen_project.invoke(
-        input={
+        input = {
             "hopeJob": userinfo.hopeJob,
-            "mainStack": userinfo.mainStack,
-            "educations": userinfo.educations,
-            "projects": userinfo.projects,
+            "mainStack": userinfo.mainStack, 
+            "educations": userinfo.educations, 
+            "projects": userinfo.projects, 
             "prizes": userinfo.prizes,
             "activities": userinfo.activities,
             "certificates": userinfo.certificates,
