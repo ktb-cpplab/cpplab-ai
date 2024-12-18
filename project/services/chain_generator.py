759db4867f103f7b5c486efc69008dacb3978ab7
CheckerBoard <hyeong8465@naver.com>
2024-12-18 15:06:39 +0900

    chores: 변수명 변경

--- project/services/chain_generator.py
+++ project/services/chain_generator.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import JsonOutputParser
 from langchain import hub
-from models.project_info import Project, Jobposting, Theme
+from models.project_info import *
 
 # 채용공고 요약 chain
 def jobposting_summary_chain():
@@ -15,7 +15,7 @@ def jobposting_summary_chain():
 def gentheme_chain():
     gen_prompt = hub.pull("gen_subject")
     gen_llm = ChatOpenAI(model="gpt-4o-mini")
-    parser = JsonOutputParser(pydantic_object=Theme)
+    parser = JsonOutputParser(pydantic_object=Themes)
     gen_prompt = gen_prompt.partial(format_instructions=parser.get_format_instructions())
     return gen_prompt | gen_llm | parser
 
@@ -28,8 +28,8 @@ def gendetails_chain():
     return gen_prompt | gen_llm | parser
 
 # 프로젝트 재생성 chain
-def create_regen_chain():
-    regen_prompt = hub.pull("regen_prompt")
+def regendetails_chain():
+    regen_prompt = hub.pull("regen_tasks")
     regen_llm = ChatOpenAI(model="gpt-4o-mini")
     parser = JsonOutputParser(pydantic_object=Project)
     regen_prompt = regen_prompt.partial(format_instructions=parser.get_format_instructions())
